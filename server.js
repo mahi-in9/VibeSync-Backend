50267d5b02095c662343ba734904afccd718347e
mahi-in9 <mahithegreat19@gmail.com>
2025-10-26 14:33:13 +0530

    push code

--- server.js
+++ server.js
@@ -23,7 +23,15 @@ connectDB();
 const app = express();
 
 // --- Middleware ---
-app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+// app.use(cors({ origin: process.env.FRONTEND_URL || "*" }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(apiRateLimiter);
